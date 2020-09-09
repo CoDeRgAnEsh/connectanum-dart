7f3753cc19067e33e73f9bb3808524ffcd0bab10
Ganesh kumar T K <ganeshkumartk@outlook.com>
2020-09-09 15:30:26 +0530

    Support for Event retention

--- lib/src/message/subscribe.dart
+++ lib/src/message/subscribe.dart
@@ -15,9 +15,10 @@ class SubscribeOptions {
   static final String MATCH_PLAIN = null;
   static final String MATCH_PREFIX = 'prefix';
   static final String MATCH_WILDCARD = 'wildcard';
+  bool get_retained = false;
 
   String match;
   String meta_topic;
 
-  SubscribeOptions({this.match, this.meta_topic});
+  SubscribeOptions({this.match, this.meta_topic, this.get_retained});
 }
